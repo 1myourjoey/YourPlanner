bf9978e01c01b2d82d048f9b43393ba9ab117cb1
kiseob-yoon <water9371@gmail.com>
2024-06-07 18:38:59 +0900

    header update
    #B2DDEF

--- src/main/frontend/src/components/js/MainLayout.jsx
+++ src/main/frontend/src/components/js/MainLayout.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { Outlet, Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import Weather from './Weather';
+import DummyFooter from './DummyFooter';
+import '../css/header.css';
 
 const MainLayout = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -24,30 +26,34 @@ const MainLayout = () => {
           const loggedIn = sessionStorage.getItem('loginId') !== null; // 예시: 세션 스토리지에 loginId가 있는지 확인
           setIsLoggedIn(loggedIn);
       }, []);
-    return (
+      return (
         <div className="App">
-            <header className="container">
+            <header className="container header">
+            <div className="row">
+                <h3 className="logo"></h3>
+                <div className="col auth-container">
+                <div className="auth-links">
+                    {isLoggedIn ? (
+                    <>
+                        <Link to="/" onClick={handleLogout}>로그아웃</Link>
+                    </>
+                    ) : (
+                    <>
+                        <Link to="/login">로그인</Link> <Link to="/signup">회원가입</Link>
+                    </>
+                    )}
+                    {isLoggedIn &&<Link to="/mypage">마이페이지</Link>}
+                </div>
+                </div>
+            </div>
+            </header>
+
                 <div className="col">
                     <h1>YOUR PLANNER</h1>
                     <h4>유플과 시작하는 여행 !</h4>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <div className="auth-links">
-                            {isLoggedIn ? (
-                                <>
-                                    <Link to="/" onClick={handleLogout}>로그아웃</Link>
-                                </>
-                            ) : (
-                                <>
-                                    <Link to="/login">로그인</Link> | <Link to="/signup">회원가입</Link>
-                                </>
-                            )}
-                        </div>
-                        {isLoggedIn && <div><Link to="/mypage">마이페이지</Link></div>}
-                    </div>
-                </div>
-            </header>
+
+            
             <section className="container">
                 <div className="row">
                     <div className="col">
@@ -56,6 +62,8 @@ const MainLayout = () => {
                 </div>
             </section>
             <Weather />
+            <DummyFooter />            
+
         </div>
     );
 };

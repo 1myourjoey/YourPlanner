de576608e8e5bcbe45e28f87c620f53e57b11da2
syO-Oii <as15421@naver.com>
2024-06-09 11:55:43 +0900

    선택정보 저장

---
+++ src/main/frontend/src/components/js/SavePlanInfo.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const SaveTourInfo = ({ startDate, endDate, departure, destination, selectedItems }) => {
+
+    const handleClick = () => {
+        console.log('Selected Items:', selectedItems);
+        console.log('Start Date:', startDate);
+        console.log('End Date:', endDate);
+        console.log('Departure:', departure);
+        console.log('Destination:', destination);
+
+        /* 컨트롤러로 api값 전송 */
+        fetch('/api/savePlan', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                startDate: startDate,
+                endDate: endDate,
+                departure: departure,
+                destination: destination,
+                selectedItems:  selectedItems
+            })
+
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log(data); // 서버에서 받은 응답을 콘솔에 출력
+            })
+            .catch(error => {
+                console.error('Error sending selected items to backend:', error);
+            });
+    };
+
+    return(
+        <div>
+            <button onClick={handleClick}>저장</button>
+        </div>
+    );
+}
+
+export default SaveTourInfo;

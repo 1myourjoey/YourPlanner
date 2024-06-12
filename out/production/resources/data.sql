628b6b944a2bd08a95164ec038d06f0be927a803
syO-Oii <as15421@naver.com>
2024-06-12 12:30:35 +0900

    sql 값 수정

--- out/production/resources/data.sql
+++ out/production/resources/data.sql
@@ -15,7 +15,7 @@ VALUES (2, 'Title', '2024-06-06T12:00:00', '2024-06-07T12:00:00', '파주', '강
 
 
 
-
+/*
 -- Accommodation 테이블에 데이터 삽입
 INSERT INTO Accommodation (ACCNO, SAVENO, USERNO, ACCNAME, ACCADDRESS, ACCIMG)
 VALUES (1, 1, 1, 'Accommodation A', '123 Main St', 'http://example.com/accommodation_img.jpg');
@@ -25,9 +25,9 @@ INSERT INTO RESTAURANT (RESNO, SAVENO, USERNO, RESNAME, RESADDRESS, RESIMG)
 VALUES (1, 1, 1, 'Restaurant X', '456 OAK ST', 'http://example.com/restaurant_img.jpg');
 
 -- Transportation 테이블에 데이터 삽입
-INSERT INTO TRANSPORTATION (TRANS_NO, SAVE_NO, USER_NO, TRANS_NAME, FIRST_PLACE, END_PLACE, FIRST_TIME, END_TIME)
+INSERT INTO TRANSPORTATION (TRANSNO, SAVENO, USERNO, TRANSNAME, FIRSTPLACE, ENDPLACE, FIRSTTIME, ENDTIME)
 VALUES (1, 1, 1, 'Bus', 'CITY A', 'CITY B', 20240606182600, 20240606154000);
 
 -- Tour 테이블에 데이터 삽입
 INSERT INTO TOUR (TOURNO, SAVENO, USERNO, TOURNAME, TOURADDRESS, TOURIMG)
-VALUES (1, 1, 1, 'Tour Y', '789 ELM ST', 'http://example.com/tour_img.jpg');
+VALUES (1, 1, 1, 'Tour Y', '789 ELM ST', 'http://example.com/tour_img.jpg');*/
